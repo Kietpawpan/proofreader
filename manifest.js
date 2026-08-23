9cf61c9831edef2c6be57e716128de25831774f7
Monte Kietpawpan <kietpawpan@gmail.com>
2026-08-24 00:38:10 +0700

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -7,12 +7,12 @@
   "theme_color": "#6b21a8",
   "icons": [
     {
-      "src": "/App.png",
+      "src": "/App.PNG",
       "sizes": "192x192",
       "type": "image/png"
     },
     {
-      "src": "/App.png",
+      "src": "/App.PNG",
       "sizes": "512x512",
       "type": "image/png"
     }
