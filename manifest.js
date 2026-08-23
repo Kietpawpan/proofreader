be427a691494a15c8c4425199a1677710337ef53
Monte Kietpawpan <kietpawpan@gmail.com>
2026-08-24 00:23:42 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "Professional Proofreader",
+  "short_name": "Proof",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#f3f4f6",
+  "theme_color": "#6b21a8",
+  "icons": [
+    {
+      "src": "img/App.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "img/App.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
